265994c9eff26020d198b1c5a1636f6379a7a091
顾晓勇 <guxiaoyong888@gmail.com>
2017-02-25 02:02:50 +0800

    实现TLSv1.2 client-server-demo

--- src/main/java/pers/guxiaoyong/ssl/client/EchoClient.java
+++ src/main/java/pers/guxiaoyong/ssl/client/EchoClient.java
@@ -1,15 +1,63 @@
 package pers.guxiaoyong.ssl.client;
 
-import javax.net.ssl.SSLSocket;
-import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.*;
 import java.io.*;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 public class EchoClient {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, IOException, CertificateException, UnrecoverableKeyException {
+        // key store相关信息
+        String keyName = "target/classes/pers/guxiaoyong/ssl/client/mySrvKeystore";
+        char[] keyStorePwd = "123456".toCharArray();
+        char[] keyPwd = "123456".toCharArray();
+        // port 协议类型
+        String serverAddress = "localhost";
+        int port = 9999;
+        String protocol = "TLSv1.2";
+
+        // 装载当前目录下的key store. 可用jdk中的keytool工具生成keystore
+        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        InputStream ksIs = new FileInputStream(keyName);
         try {
-            SSLSocketFactory sslsocketfactory = (SSLSocketFactory) SSLSocketFactory.getDefault();
-            SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket("localhost", 9999);
+            ks.load(ksIs, keyStorePwd);
+        } finally {
+            if (ksIs != null) {
+                ksIs.close();
+            }
+        }
+        // 初始化key manager factory
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        kmf.init(ks, keyPwd);
+
+        // 初始化ssl context
+        SSLContext sSLContext = SSLContext.getInstance(protocol);
+        sSLContext.init(kmf.getKeyManagers(), new TrustManager[]{new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        }}, new SecureRandom());
+
+        SSLSocketFactory sslsocketfactory = sSLContext.getSocketFactory();
+
+        try {
+            SSLSocket sslsocket = (SSLSocket) sslsocketfactory.createSocket(serverAddress, port);
+            sslsocket.setEnabledCipherSuites(sSLContext.getServerSocketFactory().getSupportedCipherSuites());
+            sslsocket.setEnabledProtocols(new String[] {"TLSv1", "TLSv1.1", "TLSv1.2", "SSLv3"});
+
             InputStream inputstream = System.in;
             InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
             BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
--- src/main/java/pers/guxiaoyong/ssl/server/EchoServer.java
+++ src/main/java/pers/guxiaoyong/ssl/server/EchoServer.java
@@ -1,23 +1,66 @@
 package pers.guxiaoyong.ssl.server;
 
-import javax.net.ssl.SSLServerSocket;
-import javax.net.ssl.SSLServerSocketFactory;
-import javax.net.ssl.SSLSocket;
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import javax.net.ssl.*;
+import java.io.*;
+import java.net.Socket;
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 
 public class EchoServer {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException, KeyStoreException, IOException, UnrecoverableKeyException, CertificateException, KeyManagementException {
+        // key store相关信息
+        String keyName = "target/classes/pers/guxiaoyong/ssl/server/mySrvKeystore";
+        char[] keyStorePwd = "123456".toCharArray();
+        char[] keyPwd = "123456".toCharArray();
+        // port 协议类型
+        int port = 9999;
+        String protocol = "TLSv1.2";
+        // 装载当前目录下的key store. 可用jdk中的keytool工具生成keystore
+        KeyStore ks = KeyStore.getInstance(KeyStore.getDefaultType());
+        InputStream ksIs = new FileInputStream(keyName);
         try {
-            SSLServerSocketFactory sslserversocketfactory =
-                    (SSLServerSocketFactory) SSLServerSocketFactory.getDefault();
-            SSLServerSocket sslserversocket =
-                    (SSLServerSocket) sslserversocketfactory.createServerSocket(9999);
-            SSLSocket sslsocket = (SSLSocket) sslserversocket.accept();
+            ks.load(ksIs, keyStorePwd);
+        } finally {
+            if (ksIs != null) {
+                ksIs.close();
+            }
+        }
+        // 初始化key manager factory
+        KeyManagerFactory kmf = KeyManagerFactory.getInstance(KeyManagerFactory.getDefaultAlgorithm());
+        kmf.init(ks, keyPwd);
 
-            InputStream inputstream = sslsocket.getInputStream();
+        // 初始化ssl context
+        SSLContext sSLContext = SSLContext.getInstance(protocol);
+        sSLContext.init(kmf.getKeyManagers(), new TrustManager[]{new X509TrustManager() {
+            @Override
+            public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        }}, new SecureRandom());
+
+        try {
+            SSLServerSocketFactory sslserversocketfactory = sSLContext.getServerSocketFactory();
+            
+            SSLServerSocket sslserversocket = (SSLServerSocket) sslserversocketfactory.createServerSocket(port);
+            sslserversocket.setEnabledCipherSuites(sSLContext.getServerSocketFactory().getSupportedCipherSuites());
+            sslserversocket.setEnabledProtocols(new String[] {"TLSv1", "TLSv1.1", "TLSv1.2", "SSLv3"});
+
+            Socket client = sslserversocket.accept();
+            System.out.println(client.getRemoteSocketAddress());
+            //获取流
+            InputStream inputstream = client.getInputStream();
             InputStreamReader inputstreamreader = new InputStreamReader(inputstream);
             BufferedReader bufferedreader = new BufferedReader(inputstreamreader);
 
